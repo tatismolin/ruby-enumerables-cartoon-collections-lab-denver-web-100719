c67839299b88ba6d2f2751691817bb64f1c5c5c3
Tatiana Smolin <tatismolin@gmail.com>
2019-09-20 04:03:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types.index)
-
+  array.include?(cheese_types)
+  puts array.index{|n| n == cheese_types}
 end
