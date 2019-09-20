31b79eef960e3de58090ad84dfa177c45a6fc50f
Tatiana Smolin <tatismolin@gmail.com>
2019-09-20 03:08:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index{|n| n}.
-  puts array
+  array
 end
 
 def summon_captain_planet# code an argument here
