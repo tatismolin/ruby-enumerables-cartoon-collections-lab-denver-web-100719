e2c495e7c2d147f3aca72c521ea7d62c2e2e7bcc
Tatiana Smolin <tatismolin@gmail.com>
2019-09-20 03:32:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.each{|n| n.capitalizes + "!"}
+  array.each{|n| n.upcase + "!"}
 end
 
 def long_planeteer_calls# code an argument here
