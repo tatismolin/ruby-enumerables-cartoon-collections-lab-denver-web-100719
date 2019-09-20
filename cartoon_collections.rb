2ba8122158a84ce5eb7e9783ae99cd250db80b48
Tatiana Smolin <tatismolin@gmail.com>
2019-09-20 03:50:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any?{|n| n}
+  array.any?{|n| n.length > 4}
 end
 
 def find_the_cheese# code an argument here
