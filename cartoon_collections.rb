3d653da24d186d93e83ae8390f4a361220a468dc
Tatiana Smolin <tatismolin@gmail.com>
2019-09-20 03:43:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,9 @@ def long_planeteer_calls(array)# code an argument here
   # Your code here
   array.each{|n| n}
   if array.length > 4
-    puts array
+    puts true
   else
-    puts nil
+    puts false
   end
 end
 
