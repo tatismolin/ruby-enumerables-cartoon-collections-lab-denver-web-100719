50c82b45e9d6ab6f3e8bc73d12ec4913cf3880e5
Tatiana Smolin <tatismolin@gmail.com>
2019-09-20 03:53:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(array)# code an argument here
   array.any?{|n| n.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_types)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.include{|n| n}
 end
